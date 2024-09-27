6068ff1fa8321527b0d25d336a8ec3c1745f5552
contiyu <131341345+continye@users.noreply.github.com>
2024-09-27 14:12:03 +0800

    Update request.js

--- src/request.js
+++ src/request.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-
+    import OpenAI from "openai";
 const request = axios.create({
     baseURL:"https://images.pexels.com/photos/23698640/pexels-photo-23698640.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     timeout:1000
